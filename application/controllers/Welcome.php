670f1d0650e0763a8029538c2b4189e5d20bad73
gmorae <gmmartins06@gmail.com>
2019-07-12 00:48:17 -0300

    Colocando para o usuario ver

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,19 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * Contrlador responsavel por mostrar a area do usuario
+	 * @author Gabriel Moraes Martins 
+	 * @link https://github.com/gmorae/USA
 	 */
 	public function __construct()
 	{
@@ -24,9 +14,7 @@ class Welcome extends CI_Controller {
 		$this->load->database();
 		$this->load->library(['ion_auth', 'form_validation']);
 		$this->load->helper(['url', 'language']);
-
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
-
 		$this->lang->load('auth');
 		$this->load->view('common/header');
 		$this->load->view('common/footer');
@@ -38,15 +26,15 @@ class Welcome extends CI_Controller {
 	}
 	public function ccb()
 	{
-		
+		$this->load->view('usuario/ccb/form');
 	}
 	public function pessoal()
 	{
-		
+		$this->load->view('usuario/pessoal/form');
 	}
 	public function saude()
 	{
-		
+		$this->load->view('usuario/saude/form');	
 	}
 
 }
