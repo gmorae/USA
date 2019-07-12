758be7c502d2bd91451041faf84a72ee80947126
gmorae <gmmartins06@gmail.com>
2019-07-12 00:37:46 -0300

    front end questionario

---
+++ application/views/usuario/saude/form.php
@@ -0,0 +1,30 @@
+<form method="POST">
+    <div class="container">
+        <div class="card mt-3">
+            <div class="card-header">
+                <h3 class="text-center">Dados Congregação</h3>
+            </div>
+            <div class="row col-md-12 mx-auto ">
+                <div class="md-form col-md-4">
+                    <select class="browser-default custom-select">
+                        <option selected>Tem medo de altura ?</option>
+                        <?= $funcao ?>
+                    </select>
+                </div>
+                <div class="md-form col-md-4">
+                    <select class="browser-default custom-select">
+                        <option selected>Tem medo de altura ?</option>
+                        <?= $funcao ?>
+                    </select>
+                </div>
+                <div class="md-form col-md-4">
+                    <select class="browser-default custom-select">
+                        <option selected>Tem medo de altura ?</option>
+                        <?= $funcao ?>
+                    </select>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-outline-cyan waves-effect">Enviar</button>
+        </div>
+    </div>
+</form>
